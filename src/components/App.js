37bf7c1cafae77feb0e8854bf1c75bc8d65b4f59
Arpit <arpit@interfacem.com>
2020-10-02 00:58:26 +0530

    - Setup state for editor component - Setup editor component - Styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Editor from "./Editor";
 
 function App() {
+
+    const [html, setHtml] = useState('');
+    const [css, setCss] = useState('');
+    const [javascript, setJavascript] = useState('');
+    const [srcDoc, setSrcDoc] = useState('');
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setSrcDoc(`
+                <html>
+                    <body>${html}</body>
+                    <style>${css}</style>
+                    <script>${javascript}</script>
+                </html>    
+                `)
+        }, 1000);
+
+        return () => clearTimeout(timeout)
+
+    }, [html, css, javascript]);
+
+
   return (
     <>
         <div className="panel top-panel">
-            <Editor/>
-            <Editor/>
-            <Editor/>
+            <Editor
+                language="xml"
+                displayName="HTML"
+                value={html}
+                onChange={setHtml}
+            />
+            <Editor
+                language="css"
+                displayName="CSS"
+                value={css}
+                onChange={setCss}
+            />
+            <Editor
+                language="javascript"
+                displayName="Javascript"
+                value={javascript}
+                onChange={setJavascript}
+            />
         </div>
         <div className="panel bottom-panel">
             <iframe
+                srcDoc={srcDoc}
                 title="output"
                 sandbox="allow-scripts"
                 src=""
-                frameborder="0"
+                frameBorder="0"
                 width="100%"
                 height="100%"
             />
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+// Codemirror css
+import 'codemirror/lib/codemirror.css';
+//Editor css
+import 'codemirror/theme/material.css';
+// Languages
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
+// Editor
+import {Controlled as ControlledEditor} from "react-codemirror2";
 
-const Editor = () => {
+
+const Editor = (props) => {
+
+    const {
+        displayName,
+        language,
+        value,
+        onChange
+    } = props;
+
+    const [open, setOpen] = useState(true);
+
+   function handleChange (editor, data, value){
+       onChange(value)
+   }
+
     return (
-        <div>
-            This will be the editor.
+        <div className={`editor-container ${open ? '' : 'collapsed'}`}>
+            <div className="editor-title">
+                {displayName}
+                <button onClick={() => setOpen(prevOpen => !prevOpen)}>o</button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    lineNumbers: true,
+                    theme: 'material'
+
+                }}
+            />
         </div>
     );
 };
